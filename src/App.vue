68b615917b9000dced26cd219c5819ce8395a519
NicolaSforza93 <nicola.sforza93@gmail.com>
2024-01-15 16:11:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>VITE-BOOLFOLIO</h1>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
